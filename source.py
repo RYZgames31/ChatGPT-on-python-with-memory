a5796ab0aeaafbbdc5d1b9f1d7a87305d3b8372c
RYZgames31 <168097427+RYZgames31@users.noreply.github.com>
2025-09-22 17:11:41 +0300

    Update source.py
    Make memory code and credit

--- source.py
+++ source.py
@@ -6,24 +6,27 @@ def menu():
     os.system('cls' if os.name == 'nt' else 'clear')    
     print(Colorate.Horizontal(Colors.red_to_white, (r""" 
 . __             __ 
-_/  |___  _  __ |__|
-\   __\ \/ \/ / |  |
- |  |  \     /  |  | made by tea with trojan
- |__|   \/\_/\__|  |teawithtrojan.404.mn/discord 
+_/  |___  _  __ |__| made by tea with trojan
+\   __\ \/ \/ / |  | teawithtrojan.404.mn/discord
+ |  |  \     /  |  | fork made by RYZgames31
+ |__|   \/\_/\__|  | github.com/RYZgames31
             \______|
     """).strip()))
 
 client = Client()
+messages_list = []
 while True:
     menu()
     message = input("message: ")
+    messages_list.append({"role": "user", "content": message})
     menu()
     print("Loading...")
     response = client.chat.completions.create(
         model="gpt-4.1",  # Try "gpt-4o", "deepseek-v3", etc.
-        messages=[{"role": "user", "content": message}],
+        messages=messages_list,
         web_search=False
     )
     menu()
+    messages_list.append({"role": "assistant", "content": response.choices[0].message.content})
     print(response.choices[0].message.content)
     os.system("pause")
